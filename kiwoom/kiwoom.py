6e9d5812c07cdf194fa6bc00edd37434e9001c0b
hojinlee <infohojin@gmail.com>
2023-12-09 17:24:39 +0900

    init

---
+++ kiwoom/kiwoom.py
@@ -0,0 +1,3 @@
+class Kiwoom():
+    def __init__(self) -> None:
+        print("Kiwwom 클래스 입니다.")
---
+++ ui/ui.py
@@ -0,0 +1,6 @@
+from kiwoom.kiwoom import *
+
+class Ui_class():
+    def __init__(self):
+        print("ui 클래스")
+        Kiwoom()        
