26d9e32d620f366fccea8c641c049560aac37342
hojinlee <infohojin@gmail.com>
2023-12-11 19:37:05 +0900

    login and error_code

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,3 +1,42 @@
-class Kiwoom():
-    def __init__(self) -> None:
+from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
+from config.errorCode import *
+
+
+class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
         print("Kiwwom 클래스 입니다.")
+        
+        ## 이벤트 루프 모음
+        self.login_event_loop = None
+        ## ===
+        
+        self.get_ocx_instance()
+        self.event_slots()
+        self.signal_login_commConnect()
+        
+        
+    def get_ocx_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+        
+        
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+    
+    def login_slot(self, errCode):
+        print(errors(errCode))
+        
+        
+        ## 0 일때 정상
+        self.login_event_loop.exit()
+        
+    def signal_login_commConnect(self):
+        self.dynamicCall("commConnect()")
+        
+        self.login_event_loop = QEventLoop()
+        self.login_event_loop.exec_()
+        
+         
+        
+        
--- ui/ui.py
+++ ui/ui.py
@@ -1,6 +1,14 @@
 from kiwoom.kiwoom import *
 
+#from PyQt5.QtWidgets import *
+import sys
+from PyQt5.QtWidgets import *
+
 class Ui_class():
     def __init__(self):
         print("ui 클래스")
-        Kiwoom()        
+        
+        self.app = QApplication(sys.argv)        
+        self.kiwoom = Kiwoom()
+        
+        self.app.exec_()       
